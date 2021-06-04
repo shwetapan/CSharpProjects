c5aa5a641fbf62f7ba4d6d64db1007a4db3bdf00
shwetapan <shwetapandey9207@gmail.com>
2021-06-04 23:35:40 +0530

    Oder Restaurant Food First Commit

---
+++ OdeToFood/OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,20 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+   public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updateRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int Id);
+        int GetCountOfRestaurants();
+        int Commit();
+    }
+
+}
---
+++ OdeToFood/OdeToFood/OdeToFood/ViewComponents/RestauranrCountViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.ViewComponents
+{
+    public class RestauranrCountViewComponent:ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+
+        public RestauranrCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var count = restaurantData.GetCountOfRestaurants();
+            return View(count);
+        }
+    }
+}
